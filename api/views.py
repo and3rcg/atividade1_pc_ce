bfecc133eb49ed1eaa2fe0655cbe412e3bb1d080
Anderson Caminha <geo.anders1@gmail.com>
2022-05-10 02:24:33 -0300

    fix: removida ação sem uso e imports vazios
    Signed-off-by: Anderson Caminha <geo.anders1@gmail.com>

--- api/views.py
+++ api/views.py
@@ -1,7 +1,4 @@
 from rest_framework.viewsets import ModelViewSet
-from rest_framework import status
-from rest_framework.decorators import action
-from rest_framework.response import Response
 
 from api.serializers import ArmaSerializer, CalibreSerializer, MunicaoSerializer, ObjetoSerializer, ObjetoTipoSerializer
 from .models import Calibre, ObjetoTipo, Objeto, Arma, Municao
@@ -10,12 +7,6 @@ from .models import Calibre, ObjetoTipo, Objeto, Arma, Municao
 class ArmaViewset(ModelViewSet):
     queryset = Arma.objects.all()
     serializer_class = ArmaSerializer
-
-    @action(methods=['post'], detail=True)
-    def disparar(self, request, pk=None):
-        arma = self.get_object()
-        serializer = ArmaSerializer(arma)
-        return Response(data=serializer.data, status=status.HTTP_418_IM_A_TEAPOT)
 
 
 class CalibreViewset(ModelViewSet):
